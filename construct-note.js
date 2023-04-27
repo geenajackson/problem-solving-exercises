9d3e7d1eedde89c317bdb33ff6fb1b55f0272aac
Geena Jackson <geena.jackson96@gmail.com>
2023-04-27 13:10:34 -0500

    implement constructNote via freq counter

--- construct-note.js
+++ construct-note.js
@@ -1,3 +1,30 @@
 // add whatever parameters you deem necessary
-function constructNote() {
+function constructNote(msg, letters) {
+    //if length of msg is bigger than letters, there can't be enough letters
+    if (msg.length > letters.length) return false;
+    //if msg is empty, return true
+    if (!msg) return true;
+
+    let msgFreq = freqCounter(msg);
+    let lettersFreq = freqCounter(letters);
+
+    //if the freq of a letter of msg is ever bigger than freq of a letter in letters, cannot be enough letters
+    for (let letter of msg) {
+        if (msgFreq[letter] > lettersFreq[letter]) {
+            return false;
+        }
+    }
+
+    return true;
+
+}
+
+//get the freq of letters of each string
+function freqCounter(str) {
+    const freq = {};
+    for (let letter of str) {
+        freq[letter] ? freq[letter]++ : freq[letter] = 1;
+    }
+
+    return freq;
 }
