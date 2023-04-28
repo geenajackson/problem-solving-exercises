3f50109a4ce5bb5b5beb752d7a75e2fa032f0fd3
Geena Jackson <geena.jackson96@gmail.com>
2023-04-28 09:11:27 -0500

    implement twoArrayObject func

--- two-array-object.js
+++ two-array-object.js
@@ -1,3 +1,14 @@
 // add whatever parameters you deem necessary
-function twoArrayObject() {
+function twoArrayObject(keys, values) {
+    //create an object
+    let obj = {};
+
+    //loop through lengths of keys
+    for (let i = 0; i < keys.length; i++) {
+        //if there exists a value at i, add that value to key; else add null
+        values[i] ? obj[keys[i]] = values[i] : obj[keys[i]] = null;
+    }
+
+    //return object
+    return obj;
 }
