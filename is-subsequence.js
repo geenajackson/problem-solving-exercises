e1c782fb4bac40e2de60a02d19d9ee0adc8a7d50
Geena Jackson <geena.jackson96@gmail.com>
2023-04-28 11:05:26 -0500

    implement isSubsequence using mult pointers

--- is-subsequence.js
+++ is-subsequence.js
@@ -1,3 +1,20 @@
 // add whatever parameters you deem necessary
-function isSubsequence() {
+function isSubsequence(str1, str2) {
+    //follow chars of str1
+    let charIdx = 0;
+
+    //if chars of str1 match curr char of str2, add to str1 counter
+    for (let i = 0; i < str2.length; i++) {
+        if (str1[charIdx] === str2[i]) {
+            charIdx++;
+        }
+    }
+
+    //if all chars were found, the charIdx will equal the length of the string
+    if (charIdx === str1.length) {
+        return true;
+    }
+
+    //else, return false
+    return false;
 }
