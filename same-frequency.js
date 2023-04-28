1448e28d6c75568cd67b29bd5808af3b446e429d
Geena Jackson <geena.jackson96@gmail.com>
2023-04-28 09:30:28 -0500

    implement sameFrequency via freq counter

--- same-frequency.js
+++ same-frequency.js
@@ -1,3 +1,27 @@
 // add whatever parameters you deem necessary
-function sameFrequency() {
+function sameFrequency(int1, int2) {
+    let freq1 = freqCounter(int1);
+    let freq2 = freqCounter(int2);
+
+    if (Object.keys(freq1).length !== Object.keys(freq2).length) {
+        return false;
+    }
+
+    for (let key of Object.keys(freq1)) {
+        if (freq1[key] !== freq2[key]) {
+            return false;
+        }
+    }
+
+    return true;
+}
+
+function freqCounter(nums) {
+    let obj = {};
+    let arr = nums.toString().split("")
+    for (let num of arr) {
+        obj[num] ? obj[num]++ : obj[num] = 1;
+    }
+
+    return obj;
 }
