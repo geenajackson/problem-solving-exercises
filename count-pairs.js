18d73d47c587ec1773914860c3d9ee9df74326b8
Geena Jackson <geena.jackson96@gmail.com>
2023-04-28 12:05:41 -0500

    implement countPairs

--- count-pairs.js
+++ count-pairs.js
@@ -1,3 +1,18 @@
 // add whatever parameters you deem necessary
-function countPairs() {
+function countPairs(integers, sum) {
+    //keeps track of pairs
+    let pairs = 0;
+
+    //go through each value of array; j is increased since no need to check previous vals
+    for (let i = 0; i < integers.length; i++) {
+        for (let j = i + 1; j < integers.length; j++) {
+            if (integers[i] + integers[j] === sum) {
+                pairs++;
+                break;
+            }
+        }
+    }
+
+    return pairs;
 }
+
